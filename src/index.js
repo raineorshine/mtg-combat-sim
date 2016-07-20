95c9620bd30a3d257e1dad126dabda9071ea9c5f
Raine Revere <raineorshine@gmail.com>
2016-07-20 16:07:58 -0600

    Add loading screen.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as mtg from 'mtgsdk'
 const r = require('r-dom')
-import { div, img, hr } from 'r-dom'
-import { filter, random, sampleSize } from 'lodash'
+import { div, img, hr, span } from 'r-dom'
+import { filter, random, repeat, sampleSize } from 'lodash'
 import { merge, propEq } from 'ramda'
 
 const ALL_COLORS = ['White', 'Blue', 'Black', 'Red', 'Green']
@@ -17,6 +17,10 @@ const LIFE_MIN = 1
 const LIFE_MAX = 20
 
 const log = console.log.bind(console)
+
+let state = {
+  loading: { count: 0 }
+}
 
 const generateBoard = allCards => {
 
@@ -45,17 +49,12 @@ const generateBoard = allCards => {
   return { hand, creatures, lands, life: random(LIFE_MIN, LIFE_MAX) }
 }
 
-const render = async () => {
+const render = () => {
 
-  // fetch cards
-  const cards = await mtg.card.where({ set: 'EMN', pageSize: 500 })
-
-  // generate board
-  const board1 = generateBoard(cards)
-  const board2 = generateBoard(cards)
+  const { loading, board1, board2 } = state
 
   ReactDOM.render(
-    r(Game, { board1, board2 }),
+    r(Game, { loading, board1, board2 }),
     document.getElementById('app')
   )
 }
@@ -64,21 +63,25 @@ const render = async () => {
  * Components
  **************************************/
 
-const Game = ({ board1, board2 }) => {
+const Game = ({ loading, board1, board2 }) => {
   return div({ className: 'game' }, [
-    div({ className: 'player' }, [
+    loading ? div({ className: 'loading' }, [
+      span({}, 'Loading'),
+      span({ className: 'loading-ellipsis' }, repeat('.', loading.count))
+    ]) : null,
+    board1 ? div({ className: 'player' }, [
       r(Life, board1),
       div({ className: 'board1' }, [r(Board, {
         // omit opponent's hand
         creatures: board1.creatures,
         lands: board1.lands
       })])
-    ]),
-    hr(),
-    div({ className: 'player' }, [
+    ]) : null,
+    board2 ? div({ className: 'player' }, [
+      hr(),
       r(Life, board2),
       div({ className: 'board2' }, [r(Board, board2)])
-    ])
+    ]) : null
   ])
 }
 
@@ -103,8 +106,8 @@ const Card = ({ name, imageUrl }) => {
 
 const Life = ({ life }) => {
   return div({ className: 'life' }, [
-    div({ className: 'life-label' }, ['Life']),
-    div({ className: 'life-value' }, [life])
+    div({ className: 'life-label' }, 'Life'),
+    div({ className: 'life-value' }, life)
   ])
 }
 
@@ -113,3 +116,22 @@ const Life = ({ life }) => {
  **************************************/
 
 render()
+
+const clock = setInterval(() => {
+  if(!state.loading) {
+    state.loading = { count: 0 }
+  }
+  else {
+    state.loading.count++
+    state.loading.count %= 4
+  }
+  render()
+}, 300)
+
+mtg.card.where({ set: 'EMN', pageSize: 500 }).then(cards => {
+  clearInterval(clock)
+  state.loading = null
+  state.board1 = generateBoard(cards)
+  state.board2 = generateBoard(cards)
+  render()
+})
